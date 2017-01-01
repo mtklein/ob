469f7c43ba54639e479164b3ffc5ea461c9bcab7
Mike Klein <mtklein@chromium.org>
2017-01-01 12:47:42 -0500

    do some hacking

--- demo/fib_test.c
+++ demo/fib_test.c
@@ -9,5 +9,8 @@ int main(void) {
     CHECK(fib(4) == 3);
     CHECK(fib(5) == 5);
     CHECK(fib(6) == 8);
+    CHECK(fib(7) == 13);
+    CHECK(fib(8) == 21);
+    CHECK(fib(9) == 34);
     return 0;
 }
--- demo/lib/fib.c
+++ demo/lib/fib.c
@@ -1,8 +1,40 @@
 #include "fib.h"
 
+static void square(int* a, int* b, int* c, int* d) {
+    int aa = *a * *a,
+        ab = *a * *b,
+        ac = *a * *c,
+        bc = *b * *c,
+        bd = *b * *d,
+        cd = *c * *d,
+        dd = *d * *d;
+    *a = aa + bc;
+    *b = ab + bd;
+    *c = ac + cd;
+    *d = bc + dd;
+}
+
+static void mul_q(int* a, int* b, int* c, int* d) {
+    int A = *a + *b,
+        B = *a,
+        C = *c + *d,
+        D = *c;
+    *a = A; *b = B; *c = C; *d=D;
+}
+
 int fib(int n) {
-    if (n < 2) {
-        return n;
+    if (n == 0) {
+        return 0;
     }
-    return fib(n-1) + fib(n-2);
+    int a = 1, b = 1, c = 1, d = 0;
+    for (int i = 1; i < n; ) {
+        if (i+i < n) {
+            square(&a,&b,&c,&d);
+            i += i;
+        } else {
+            mul_q(&a,&b,&c,&d);
+            i += 1;
+        }
+    }
+    return b;
 }
