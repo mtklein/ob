f75f5982cf570f2a71eee4ecdb7ee452a43f9937
Mike Klein <mtklein@chromium.org>
2017-01-02 12:54:56 -0500

    rely on search paths

--- demo/fancy_test.c
+++ demo/fancy_test.c
@@ -7,8 +7,8 @@ typedef uint32_t (adler32_fn)(uint32_t, const uint8_t*, unsigned);
 
 int main(void) {
     void* zlib = NULL;
-    if (!zlib) { zlib = dlopen("/usr/lib/libz.so",    RTLD_LAZY); }
-    if (!zlib) { zlib = dlopen("/usr/lib/libz.dylib", RTLD_LAZY); }
+    if (!zlib) { zlib = dlopen("libz.so",    RTLD_LAZY); }
+    if (!zlib) { zlib = dlopen("libz.dylib", RTLD_LAZY); }
     CHECK(zlib);
 
     adler32_fn* adler32 = (adler32_fn*)dlsym(zlib, "adler32");
